524219a93fa63f8d7fa85ae0f85353558e15b83f
Shashank Vashisht <shashankvashisht03@gmail.com>
2025-07-12 19:10:31 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+let elements = document.querySelector('#heading');// 1st element
+let allElements = document.querySelectorAll('p');// all elements
+console.log(allElements);
+console.log(elements);
+console.log(elements.tagName);
