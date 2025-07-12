5209e339c53e4a18288dbc84c48f477017abf009
Shashank Vashisht <shashankvashisht03@gmail.com>
2025-07-12 19:40:24 +0530

    DOM

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let elements = document.querySelector('#heading');// 1st element
-let allElements = document.querySelectorAll('p');// all elements
-console.log(allElements);
-console.log(elements);
-console.log(elements.tagName);
+// let elements = document.querySelector('#heading');// 1st element
+// // let allElements = document.querySelectorAll('p');// all elements
+// // console.log(allElements);
+// // console.log(elements);
+// // console.log(elements.tagName);
+// // console.dir(document.body.firstChild); // to get the first child of body
+// let div = document.querySelector('div');
+// console.dir(div);
+
+let elements = document.querySelector('h2');
+
+elements.innerText = `${elements.innerText} from Apna College students`;
